a2552025e325c9ff654fc6016e808a479f03e92f
Remco Niggebrugge <remco.niggebrugge@gmail.com>
2015-10-11 20:42:38 +0200

    slice test

--- temp/slice2.go
+++ temp/slice2.go
@@ -6,6 +6,15 @@ import (
 
 func main() {
 	a:=[]int{1,2,3,4,5,6}
-	a = append(x, a[2:]...)
 	fmt.Printf("%3d\n",a)
+	b := removeItems(a, 0,3)
+	fmt.Printf("%3d\n",a)
+	fmt.Printf("%3d\n",b)
+}
+
+func removeItems(s []int, start, end int) []int {
+	result := make([]int, len(s)+start-end)
+	at := copy(result, s[:start])
+	copy(result[at:],s[end:])
+	return result
 }
